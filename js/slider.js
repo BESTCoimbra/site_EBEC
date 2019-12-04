d579a01a15f48e552a5a614b9063c81140c70ff9
unknown <marianavsgs@gmail.com>
2019-12-04 20:33:38 +0000

    Falta Slides

---
+++ js/slider.js
@@ -0,0 +1,29 @@
+$(function() {
+    // ...
+
+    $('.customer-logos').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 1199,
+            settings:{
+                slidesToShow: 4
+            }
+        },{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
